a1a9034cd3b5bca5802d3cc65d05a3e3e0dab196
Marko Bakovic <markobakovic95@gmail.com>
2018-05-13 21:46:21 +0100

    Baseline, API projects

---
+++ grader-master-api/src/main/java/org/petlja/grader/master/api/GraderMaster.java
@@ -0,0 +1,8 @@
+/*
+ * (c) Copyright 2018 Petlja. All rights reserved.
+ */
+
+package org.petlja.grader.master.api;
+
+public interface GraderMaster {
+}
---
+++ grader-scheduler-api/src/main/java/org/petlja/grader/scheduler/api/GraderScheduler.java
@@ -0,0 +1,8 @@
+/*
+ * (c) Copyright 2018 Petlja. All rights reserved.
+ */
+
+package org.petlja.grader.scheduler.api;
+
+public interface GraderScheduler {
+}
