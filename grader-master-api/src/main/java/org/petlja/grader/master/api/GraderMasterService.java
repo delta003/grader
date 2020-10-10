39bb9380967e9569dec2c05d712a0ad9967e67bf
Marko Bakovic <mbakovic@palantir.com>
2020-10-10 21:35:18 +0100

    Introduce gradle-core

--- grader-master-api/src/main/java/org/petlja/grader/master/api/GraderMasterService.java
+++
@@ -1,8 +0,0 @@
-/*
- * (c) Copyright 2018 Petlja. All rights reserved.
- */
-
-package org.petlja.grader.master.api;
-
-public interface GraderMasterService {
-}
--- grader-master-api/src/main/java/org/petlja/grader/master/api/test/InputTestCase.java
+++
@@ -1,9 +0,0 @@
-/*
- * (c) Copyright 2018 Petlja. All rights reserved.
- */
-
-package org.petlja.grader.master.api.test;
-
-public interface InputTestCase extends TestCase {
-
-}
